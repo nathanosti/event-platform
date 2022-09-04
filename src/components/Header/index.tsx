395f83c320945201f7b003aa783f883c0694c5a3
Nathan Osti <nathanosti.job@gmail.com>
2022-09-04 14:13:05 -0300

    💄 Feat: add Roboto font family

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../index";
 export default function Header() {
   return (
     <>
-      <div className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <div className="w-full py-5 flex items-center justify-center bg-gray-700 border-be border-gray-600">
         <Logo />
       </div>
     </>
